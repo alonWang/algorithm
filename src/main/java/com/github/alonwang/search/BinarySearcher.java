bd717fe3427c580b3c26795254a7cc1d53c88bda
alonwang <wangweilong1996@gmail.com>
2020-04-14 08:15:56 +0800

    feat: part of binary search

---
+++ src/main/java/com/github/alonwang/search/BinarySearcher.java
@@ -0,0 +1,15 @@
+package com.github.alonwang.search;
+
+/**
+ * @author alonwang
+ * @date 2020/4/13 11:48 下午
+ * @description
+ * @detail
+ */
+public class BinarySearcher implements Searcher<Comparable> {
+    @Override
+    public int search(Comparable[] arr, Comparable target) {
+        //TODO
+        return -1;
+    }
+}
