c258f4b0efe24c3c4bc41effda10b252d118761f
alonwang <wangweilong1996@gmail.com>
2020-06-11 20:38:40 +0800

    TreeNode定义

---
+++ src/main/java/com/github/alonwang/tree/TreeNode.java
@@ -0,0 +1,16 @@
+package com.github.alonwang.tree;
+
+/**
+ * @author alonwang
+ * @date 2020/6/11 18:09
+ * @detail
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
