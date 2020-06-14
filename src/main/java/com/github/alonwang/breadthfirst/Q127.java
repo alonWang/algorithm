957602110d39a7bad89ff18647d70978201624a7
alonwang <wangweilong1996@gmail.com>
2020-06-14 14:26:23 +0800

    Q127: 单词接龙 V2 引用中间状态减少不必要的循环

--- src/main/java/com/github/alonwang/breadthfirst/Q127.java
+++ src/main/java/com/github/alonwang/breadthfirst/Q127.java
@@ -1,8 +1,11 @@
 package com.github.alonwang.breadthfirst;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Queue;
 import java.util.Set;
 
@@ -13,47 +16,52 @@ import java.util.Set;
  */
 public class Q127 {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        final int L = beginWord.length();
+        wordList.add(beginWord);
+        //构建映射 O(n*L)
+        Map<String, List<String>> transformers = new HashMap<>(L * wordList.size());
+        for (String s : wordList) {
+            for (int i = 0; i < L; i++) {
+                String transformer = s.substring(0, i) + "*" + s.substring(i + 1, L);
+                transformers.compute(transformer, (key, val) -> {
+                    if (val == null) {
+                        val = new ArrayList<>(4);
+                    }
+                    val.add(s);
+                    return val;
+                });
+            }
+        }
         Set<String> visited = new HashSet<>(wordList.size());
         Queue<String> queue = new LinkedList<>();
         int count = 0;
         queue.add(beginWord);
         visited.add(beginWord);
         count++;
+        //O(n*L*L)
         while (queue.size() > 0) {
             count++;
             int loopCount = queue.size();
             for (int i = 0; i < loopCount; i++) {
                 String key = queue.poll();
-                for (String s : wordList) {
-                    if (visited.contains(s)) {
-                        continue;
+                for (int j = 0; j < L; j++) {
+                    String transformer = key.substring(0, j) + "*" + key.substring(j + 1, L);
+                    for (String s : transformers.get(transformer)) {
+                        if (visited.contains(s)) {
+                            continue;
+                        }
+                        if (s.equals(endWord)) {
+                            return count;
+                        }
+                        queue.add(s);
+                        visited.add(s);
                     }
-                    if (!canConvert(key, s)) {
-                        continue;
-                    }
-                    if (s.equals(endWord)) {
-                        return count;
-                    }
-                    queue.add(s);
-                    visited.add(s);
                 }
+
             }
 
         }
         return 0;
     }
 
-    private boolean canConvert(String key, String source) {
-        int diffCount = 0;
-        for (int i = 0; i < source.length(); i++) {
-            if (key.charAt(i) != source.charAt(i)) {
-                diffCount++;
-                if (diffCount > 1) {
-                    return false;
-                }
-            }
-
-        }
-        return diffCount == 1;
-    }
 }
