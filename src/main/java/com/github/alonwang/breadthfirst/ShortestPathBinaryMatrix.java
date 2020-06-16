3384e5bd9c7ab560babb0f3398d2851fe730b314
alonwang <wangweilong1996@gmail.com>
2020-06-16 23:09:35 +0800

    Q1091: ShortestPathBinaryMatrix

--- src/main/java/com/github/alonwang/breadthfirst/ShortestPathBinaryMatrix.java
+++ src/main/java/com/github/alonwang/breadthfirst/ShortestPathBinaryMatrix.java
@@ -1,5 +1,8 @@
 package com.github.alonwang.breadthfirst;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * 在一个 N × N 的方形网格中，每个单元格有两种状态：空（0）或者阻塞（1）。
  * <p>
@@ -20,7 +23,110 @@ package com.github.alonwang.breadthfirst;
  * @detail
  */
 public class ShortestPathBinaryMatrix {
+    int N;
+
+    class Pos {
+        int x;
+        int y;
+
+        Pos up() {
+            if (y - 1 < 0) {
+                return null;
+            }
+            return new Pos(x, y - 1);
+        }
+
+        Pos down() {
+            if (y + 1 >= N) {
+                return null;
+            }
+            return new Pos(x, y + 1);
+        }
+
+        Pos left() {
+            if (x - 1 < 0) {
+                return null;
+            }
+            return new Pos(x - 1, y);
+        }
+
+        Pos right() {
+            if (x + 1 >= N) {
+                return null;
+            }
+            return new Pos(x + 1, y);
+        }
+
+        Pos leftup() {
+            if (x - 1 < 0 || y - 1 < 0) {
+                return null;
+            }
+            return new Pos(x - 1, y - 1);
+        }
+
+        Pos leftdown() {
+            if (x - 1 < 0 || y + 1 >= N) {
+                return null;
+            }
+            return new Pos(x - 1, y + 1);
+        }
+
+        Pos rightup() {
+            if (x + 1 >= N || y - 1 < 0) {
+                return null;
+            }
+            return new Pos(x + 1, y - 1);
+        }
+
+        Pos rightdown() {
+            if (x + 1 >= N || y + 1 >= N) {
+                return null;
+            }
+            return new Pos(x + 1, y + 1);
+        }
+
+        public Pos(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
     public int shortestPathBinaryMatrix(int[][] grid) {
+        N = grid.length;
+        boolean[][] visited = new boolean[N][N];
+        Queue<Pos> queue = new LinkedList<>();
+        queue.add(new Pos(0, 0));
+        int count = 0;
+        while (!queue.isEmpty()) {
+            count++;
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                Pos index = queue.poll();
+                if (index == null) {
+                    continue;
+                }
+                if (visited[index.x][index.y]) {
+                    continue;
+                }
+                visited[index.x][index.y] = true;
+                if (grid[index.x][index.y] == 1) {
+                    continue;
+                }
+                if (index.x == N - 1 && index.y == N - 1) {
+                    return count;
+                }
+                queue.add(index.left());
+                queue.add(index.right());
+                queue.add(index.up());
+                queue.add(index.down());
+                queue.add(index.leftup());
+                queue.add(index.leftdown());
+                queue.add(index.rightup());
+                queue.add(index.rightdown());
+
+
+            }
+        }
         return -1;
     }
 }
