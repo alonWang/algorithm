2b18422d44e7dfabd193e2f49731c4df531bbd83
alonwang <wangweilong1996@gmail.com>
2020-07-10 08:39:34 +0800

    动态规划 wiggleMaxLength

--- src/test/groovy/com/github/alonwang/DynamicprogramingSpecification.groovy
+++ src/test/groovy/com/github/alonwang/DynamicprogramingSpecification.groovy
@@ -1,6 +1,7 @@
 package com.github.alonwang
 
 import com.github.alonwang.dynamicprograming.Q139
+import com.github.alonwang.dynamicprograming.Q376
 import spock.lang.Specification
 
 class DynamicprogramingSpecification extends Specification {
@@ -12,5 +13,14 @@ class DynamicprogramingSpecification extends Specification {
         "leetcode"                                                                                                                                                | ["leet", "code"]                                                                                | true
         "catsandog"                                                                                                                                               | ["cats", "dog", "sand", "and", "cat"]                                                           | false
         "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab" | ["a", "aa", "aaa", "aaaa", "aaaaa", "aaaaaa", "aaaaaaa", "aaaaaaaa", "aaaaaaaaa", "aaaaaaaaaa"] | false
+    }
+
+    def "est Q376"() {
+        expect:
+        new Q376().wiggleMaxLength(nums as int[]) == result
+        where:
+        nums                                 | result
+        [1, 7, 4, 9, 2, 5]                   | 6
+        [1, 17, 5, 10, 13, 15, 10, 5, 16, 8] | 7
     }
 }
