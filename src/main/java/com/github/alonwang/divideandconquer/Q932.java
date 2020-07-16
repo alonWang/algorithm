8ac67cf16105dca18e54706618ff14c89edc3600
alonwang <wangweilong1996@gmail.com>
2020-07-16 08:10:32 +0800

    分治算法 漂亮数

---
+++ src/main/java/com/github/alonwang/divideandconquer/Q932.java
@@ -0,0 +1,37 @@
+package com.github.alonwang.divideandconquer;
+
+/**
+ * 对于某些固定的 N，如果数组 A 是整数 1, 2, ..., N 组成的排列，使得：
+ * <p>
+ * 对于每个 i < j，都不存在 k 满足 i < k < j 使得 A[k] * 2 = A[i] + A[j]。
+ * <p>
+ * 那么数组 A 是漂亮数组。
+ * <p>
+ *  
+ * <p>
+ * 给定 N，返回任意漂亮数组 A（保证存在一个）。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：4
+ * 输出：[2,1,4,3]
+ * 示例 2：
+ * <p>
+ * 输入：5
+ * 输出：[3,1,2,5,4]
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/beautiful-array
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author alonwang
+ * @date 2020/7/16 8:05 上午
+ * @detail
+ */
+public class Q932 {
+    public int[] beautifulArray(int N) {
+
+    }
+}
